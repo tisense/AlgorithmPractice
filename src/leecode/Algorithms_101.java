510f090c3e11ef630192ed89e03cd009a5581bc8
renqi <ptqyuio@gmail.com>
2021-06-04 14:57:32 +0800

    leetcode 101 对称二叉树

---
+++ src/leecode/Algorithms_101.java
@@ -0,0 +1,57 @@
+package leecode;
+
+
+import java.util.Stack;
+
+import javax.lang.model.type.MirroredTypeException;
+
+import com.sun.xml.internal.ws.api.FeatureConstructor;
+
+import leecode.Algorithms_94.TreeNode;
+
+public class Algorithms_101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public static void test(String string) {
+		String tempString = string;
+		tempString = "AAA";
+		System.out.println(string + "," + tempString);
+	}
+	/**
+	 * 对称树
+	 * symmetric  tree
+	 * @param root
+	 * @return
+	 */
+	public static boolean isSymmetric(TreeNode root) {
+		
+		//TreeNode left = root.left;
+		//TreeNode right = root.right;
+		//Stack<TreeNode> stackLeft = new Stack<TreeNode>();
+		//Stack<TreeNode> stackRight = new Stack<TreeNode>();
+		return mirro(root.left, root.right);
+
+    }
+	
+	/**
+	 * 递归
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public static boolean mirro(TreeNode left, TreeNode right) {
+		if (left != null && right != null) {
+			return left.val == right.val 
+					&& mirro(left.left, right.right) 
+					&& mirro(left.right, right.left);
+		} else {
+			return left == right;
+		}
+	}
+
+}
