e188ba60a26d8d49a1273a76d47be4ff929882d7
renqi <ptqyuio@gmail.com>
2021-06-16 11:53:28 +0800

    leetcode 111 二叉树的最小深度

---
+++ src/leecode/Algorithms_111.java
@@ -0,0 +1,32 @@
+package leecode;
+
+import leecode.Algorithms_94.TreeNode;
+
+/**
+ * 二叉树的最小深度
+ * @author renqi
+ *
+ */
+public class Algorithms_111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		
+		int leftH = minDepth(root.left);
+		int rightH = minDepth(root.right);
+		if (leftH == 0 && rightH == 0) {
+			return 1;
+		}else if( leftH != 0 && rightH != 0) {
+			return Math.min(leftH, rightH) + 1;
+		}else {
+			return Math.max(leftH, rightH) + 1;
+		}
+    }
+}
